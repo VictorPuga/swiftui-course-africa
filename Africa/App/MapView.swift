0db895b20297d13f26e0b4f6df807bf88ba4f50d
VictorPuga <victorpugaruiz@live.com.mx>
2020-11-07 18:16:16 -0700

    initial app setup and assets

---
+++ Africa/App/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  Africa
+//
+//  Created by Victor Manuel Puga Ruiz on 07/11/20.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
