b911ca0871e9d8f085dda1aae64d111d30b297c5
VictorPuga <victorpugaruiz@live.com.mx>
2020-11-07 17:48:35 -0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Victor Manuel Puga Ruiz on 07/11/20.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
